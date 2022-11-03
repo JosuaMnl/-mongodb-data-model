2bed5084b0137665b5158bd8f726b93c23a21643
JosuaMnl <josuamnalu@gmail.com>
2022-11-03 09:10:28 +0700

    Setup mahasiswaCtrl.js

--- api_server/routes/index.js
+++ api_server/routes/index.js
@@ -5,10 +5,18 @@ var router = express.Router();
 var ctrlMhs = require('../controllers/mahasiswaCtrl');
 
 // Daftarkan Routes
-// Http://localhost:3000/api/mahasiswa -> get -> all data mhs
-// Http://localhost:3000/api/mahasiswa -> post -> insert data mhs
+// http://localhost:3000/api/mahasiswa -> get -> all data mhs
+// http://localhost:3000/api/mahasiswa -> post -> insert data mhs
 router.route('/mahasiswa')
     .get(ctrlMhs.mhsList)
     .post(ctrlMhs.mhsCreate);
+
+// http://localhost:3000/api/mahasiswa/123 -> get -> get data mhs by id
+// http://localhost:3000/api/mahasiswa/123 -> put -> update data mhs
+// http://localhost:3000/api/mahasiswa/123 -> delete -> delete data mhs
+router.route('/mahasiswa/:id')
+    .get(ctrlMhs.mhsReadOne)
+    .put(ctrlMhs.mhsUpdateOne)
+    .delete(ctrlMhs.mhsDeleteOne);
     
 module.exports = router;
